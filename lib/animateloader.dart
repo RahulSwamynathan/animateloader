30751cd3a3f480b575c20dc241c9f7ee194f123b
Rahul Swamynathan <Rahul.S@customeranalytics.com>
2021-01-15 15:31:42 +0530

    Base version changes for Animation loader

--- lib/animateloader.dart
+++ lib/animateloader.dart
@@ -1,7 +1,50 @@
 library animateloader;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class AnimateLoader extends StatefulWidget {
+  final bool startAnimate;
+  final Widget child;
+
+  const AnimateLoader({Key key, @required this.startAnimate, @required this.child}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => AnimateLoaderState();
+}
+
+class AnimateLoaderState extends State<AnimateLoader> with TickerProviderStateMixin {
+  Animation<double> _fadeInFadeOut;
+  AnimationController animation;
+
+  AnimateLoaderState() {
+    animation = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+    _fadeInFadeOut = Tween<double>(begin: 0.2, end: 0.8).animate(animation);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animation.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animation.forward();
+      }
+    });
+    animation.forward();
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.startAnimate
+        ? FadeTransition(
+            opacity: _fadeInFadeOut,
+            child: AbsorbPointer(absorbing: true, child: widget.child),
+          )
+        : widget.child;
+  }
 }
